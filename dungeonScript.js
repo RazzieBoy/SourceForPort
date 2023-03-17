cabdb58e010ca49f9e3273f90b1ec5b6abbe0d62
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2023-03-17 15:16:34 +1300

    Initial commit

---
+++ dungeonScript.js
@@ -0,0 +1,15 @@
+function goTo() {
+  var inputValue = document.getElementById('dungeonInput').value
+  if (inputValue === "Hell") {
+    window.location = 'https://HellHole.rasmusnordberg2.repl.co';
+  }
+  if (inputValue === "Info") {
+    window.location = 'https://rasmus.gay';
+  }
+}
+
+document.querySelector("dungeonInput").onkeydown = e => {
+  if (e.key == "Enter") {
+    goTo();
+  }
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+function goTo() {
+  var inputValue = document.getElementById('input').value
+  if (inputValue === "Hell") {
+    window.location = 'https://HellHole.rasmusnordberg2.repl.co';
+  }
+  if (inputValue === "Dungeon") {
+    window.location = 'https://rasmus.gay/dungeon.html';
+  }
+}
+
+document.querySelector("input").onkeydown = e => {
+  if (e.key == "Enter") {
+    goTo();
+  }
+}
