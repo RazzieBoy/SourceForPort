9cb45748bff16061c4f2fbb1c76ad7b0ddb4ec5a
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2023-04-23 00:42:22 +1200

    book

--- mainScript.js
+++ mainScript.js
@@ -1,13 +1,16 @@
 function goTo() {
   var inputValue = document.getElementById('input').value
-  if (inputValue === "Hell") {
+  if (inputValue === "Hell" ||inputValue === "hell" ) {
     window.location = 'https://HellHole.rasmusnordberg2.repl.co';
   }
-  if (inputValue === "Projects") {
+  if (inputValue === "Projects" ||inputValue === "projects") {
     window.location = '/dungeon.html';
   }
-  if (inputValue === "Phone") {
+  if (inputValue === "Phone" ||inputValue === "phone") {
     window.location = '/phone.html';
+  }
+  if (inputValue === "Story" ||inputValue === "story") {
+    window.location = 'https://docs.google.com/document/d/1Tg6YNkX3W-QoCLRfZDASUulvu-HtHWTxqMG0KDDiL8I/edit?usp=sharingl';
   }
 }
 
